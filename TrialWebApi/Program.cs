a0132ff2fcd65ce0d8807f24d494a576b78015e0
md-redwan-hossain <redwan.ping@gmail.com>
2026-07-10 00:13:55 +0600

    Remove development appsettings file, update Program.cs to use ITrialService for HelloWorld endpoint, and add SharpServiceCollection.SourceGenerator project reference in csproj files.

--- TrialWebApi/Program.cs
+++ TrialWebApi/Program.cs
@@ -1,6 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using SharpServiceCollection.Generated;
+using TrialWebApi;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddGeneratedServices();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", ([FromServices] ITrialService service) => service.HelloWorld());
 
 app.Run();
